6b497f4a8d86cc61931f5ed5c51f67b0b834f064
Justin179 <justin.uob@gmail.com>
2016-11-18 22:08:11 +0800

    commit & leave

---
+++ WebServices/WebServices/CalculatorWebService.asmx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServices
+{
+    /// <summary>
+    /// Summary description for CalculatorWebService
+    /// </summary>
+    [WebService(Namespace = "http://pragimtech.com/webservices")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.None)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CalculatorWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod(MessageName="add two numbers")]
+        public int Add(int firstNum, int secondNum)
+        {
+            return firstNum+secondNum;
+        }
+
+        [WebMethod]
+        public int Add(int firstNum, int secondNum, int thirdNum)
+        {
+            return firstNum + secondNum + thirdNum;
+        }
+
+        [WebMethod(EnableSession = true)]
+        public List<string> GetCalculations()
+        {
+            if (Session["CALCULATION"] == null)
+            {
+                List<string> calculations = new List<string>();
+                calculations.Add("you have not performed any calculations");
+                return calculations;
+            }
+            else
+            {
+                return (List<String>) Session["CALCULATION"];
+            }
+
+        }
+    }
+}
